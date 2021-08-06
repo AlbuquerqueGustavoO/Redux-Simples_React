e27d83f5f170623d046470cc84147edd330e4ee4
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-08-05 21:55:45 -0300

    finalizando aplicação

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,8 +1,11 @@
 import './Intervalo.css'
 import React from 'react'
-import Card from './Card'
+import { connect } from 'react-redux'
 
-export default props => {
+import Card from './Card'
+import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numeros'
+
+function Intervalo(props) {
     const { min, max } = props
     return (
         <Card title='Intervalo de Números' red>
@@ -10,14 +13,40 @@ export default props => {
                 <span>
                     <strong>Mínimo:</strong>
                     <input type="number" value={min}
-                        onChange={e => props.onMinChanged(+e.target.value)} />
+                        onChange={e => props.alterarMinimo(+e.target.value)} />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={max}
-                        onChange={e => props.onMaxChanged(+e.target.value)} />
+                    <input type="number" value={max} 
+                    onChange={e => props.alterarMaximo(+e.target.value)} />
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+function mapDispatchToProp(dispatch) {
+    return {
+        alterarMinimo(novoNumero) {
+            // action creator ->action
+            const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+        alterarMaximo(novoNumero) {
+            // action creator ->action
+            const action = alterarNumeroMaximo(novoNumero)
+            dispatch(action)
+        },      
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProp)(Intervalo)
--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Card from './Card'
 
-export default props => {
+function Media(props) {
     const { min, max } = props
+    console.log(props)
     return (
         <Card title='Média dos Números' green>
             <div>
@@ -14,3 +17,12 @@ export default props => {
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Media)
